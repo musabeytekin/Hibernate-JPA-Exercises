02cda5ba1c9288018981bf3d1d3e211ea8ce7e8a
musabeytekin <02190201003@ogr.inonu.edu.tr>
2023-08-02 16:59:09 +0300

    project created, initial configurations and database connection configurations added

---
+++ src/test/java/com/musabeytekin/hibernatejpa/HibernatejpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musabeytekin.hibernatejpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatejpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
